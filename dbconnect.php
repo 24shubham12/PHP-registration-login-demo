bfcf6ea8ac4c1f6c6ef91625e2775ee80a5f292e
debayansamajpati <debayansamajpati@gmail.com>
2016-10-19 22:07:59 +0530

    Delete dbconnect.php

--- dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DBHOST', 'localhost');
-define('DBUSER', 'root');
-define('DBPASS', '');
-define('DBNAME', 'testdb');
-
-$con = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME) or
-        die("Error" . mysqli_error($con));
-?>
