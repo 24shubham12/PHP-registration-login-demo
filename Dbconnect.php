7aa1b9ad34a404cbb0f0f7465a8a69141f83703c
debayansamajpati <debayansamajpati@gmail.com>
2016-10-19 22:20:31 +0530

    Update Dbconnect.php

--- Dbconnect.php
+++ Dbconnect.php
@@ -1,5 +1,5 @@
 <?php
 //connect to mysql database
-$con = mysqli_connect("localhost", "root", "", "testdb") or 
+$con = mysqli_connect("localhost", "root", "", "testdb") or   //change username and password accordingly and give name of the db accordingly
         die("Error " . mysqli_error($con));
 ?>
