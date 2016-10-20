85445b6a32781679a22ce37ab02f35320b147478
debayansamajpati <debayansamajpati@gmail.com>
2016-10-20 20:21:41 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -20,12 +20,15 @@ include_once 'dbconnect.php';
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.php">My First PHP Page</a>
+            <div >
+                
+                <a class="navbar-brand" href="index.php"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home</a>
+            </div>
         </div>
         <div class="collapse navbar-collapse" id="navbar1">
             <ul class="nav navbar-nav navbar-right">
                 <?php if (isset($_SESSION['usr_id'])) { ?>
-                <li><p class="navbar-text">Signed in as <?php echo $_SESSION['usr_name']; ?></p></li>
+                <li><p class="navbar-text"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Signed in as <?php echo $_SESSION['usr_name']; ?></p></li>
                 <li><a href="logout.php">Log Out</a></li>
                 <?php } else { ?>
                 <li><a href="login.php">Login</a></li>
@@ -35,7 +38,26 @@ include_once 'dbconnect.php';
         </div>
     </div>
 </nav>
-
+    
+    <div class="container-fluid text-center">
+        <div style="margin-top: 15%;
+        margin-bottom: 18%;">
+            <h2><?php if (isset($_SESSION['usr_id'])) { ?>
+                <p>Signed in as <?php echo $_SESSION['usr_name']; ?></p>
+                <?php } ?></h2>
+            <h2>This is a demo site<br></h2>
+            <h3>Demonstrating PHP and MySQL <a href="Register.php">Registration</a> and <a href="Login.php">Login</a></h3>
+        </div>
+    </div>
+    
+    <footer class="container-fluid text-center">
+        <div style="margin-top: 2%;
+        margin-bottom: 2%;">
+  <p>Designed by  <a href="https://in.linkedin.com/in/debayan-samajpati-849799116">Debayan Samajpati</a></p>
+  </div>
+</footer>
+    
+   
 <script src="js/jquery-1.10.2.js"></script>
 <script src="js/bootstrap.min.js"></script>
 </body>
